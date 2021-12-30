715a27fc95c55e7ccb4dacd0756e6c798ffb12d3
StefanVinica <stefannikolovski996@gmail.com>
2021-12-30 13:21:03 -0500

    Meta is on all pages

--- components/Layout.js
+++ components/Layout.js
@@ -1,8 +1,14 @@
 import styles from '../styles/Home.module.css'
+import Head from 'next/head'
 
 const Layout = ({ children }) => {
     return (
       <>
+      <Head>
+        <title>Cassandra Jobs</title>
+        <meta name="We connect you to Cassandra jobs" content="Welcome to Cassandra.Jobs, a curated job board dedicated solely to Cassandra to help Apache Cassandra developers, administrators, and architects find their dream job" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
         <div className={styles.container}>
           <main className={styles.main}>
             {children}
--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,10 @@
-import Head from 'next/head'
+
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
   return (
     <div className={styles.container}>
-      <Head>
-        <title>Cassandra Jobs</title>
-        <meta name="We connect you to Cassandra jobs" content="Welcome to Cassandra.Jobs, a curated job board dedicated solely to Cassandra to help Apache Cassandra developers, administrators, and architects find their dream job" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+      
 
     </div>
   )
