66538ec648d3e7725a80fff8e3eeba6baaf3c777
StefanVinica <stefannikolovski996@gmail.com>
2022-01-11 10:44:07 -0500

    prod

--- pages/groceries.js
+++ pages/groceries.js
@@ -1,21 +1,21 @@
 import { Button, Grid } from "@material-ui/core"
 
-export const getStaticProps = async () => {
-    try {
-        const res = await fetch(`http://https://cjobs.vercel.app/api/cassandra`)
-        const groceries = await res.json()
-        return {
-            props: {
-                groceries,
-            },
-        }
-    } catch (error) {
-        console.error("Error fetching homepage data", error);
-    }
-}
+// export const getStaticProps = async () => {
+//     try {
+//         const res = await fetch(`http://localhost:3001/api/cassandra`)
+//         const groceries = await res.json()
+//         return {
+//             props: {
+//                 groceries,
+//             },
+//         }
+//     } catch (error) {
+//         console.error("Error fetching homepage data", error);
+//     }
+// }
 
-export default function Groceries({ groceries }) {
-    console.log(groceries.rows)
+export default function Groceries() {
+    // console.log(groceries.rows)
     return <>
         <Grid>
             <Button variant="contained">Click</Button>
