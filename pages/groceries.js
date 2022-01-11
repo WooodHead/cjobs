fe5d39bb67560b4b347c6180bd27bb24332aacc2
StefanVinica <stefannikolovski996@gmail.com>
2022-01-11 10:40:28 -0500

    prod

--- pages/groceries.js
+++ pages/groceries.js
@@ -2,7 +2,7 @@ import { Button, Grid } from "@material-ui/core"
 
 export const getStaticProps = async () => {
     try {
-        const res = await fetch(`http://localhost:3001/api/cassandra`)
+        const res = await fetch(`http://https://cjobs.vercel.app/api/cassandra`)
         const groceries = await res.json()
         return {
             props: {
--- pages/search.js
+++ pages/search.js
@@ -2,7 +2,7 @@ import { Button, Grid } from "@material-ui/core"
 
 export const getStaticProps = async () => {
   try {
-    const res = await fetch(`http://localhost:5000/cassandra-job-posts`)
+    const res = await fetch(`http://167.172.142.105:5000/cassandra-job-posts`)
     const jobs = await res.json()
     return {
       props: {
