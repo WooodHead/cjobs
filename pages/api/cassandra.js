94ae1d7e3e4a049efc1a3f477165272ba59c5afe
StefanVinica <stefannikolovski996@gmail.com>
2022-01-12 09:34:27 -0500

    Env vars

--- pages/api/cassandra.js
+++ pages/api/cassandra.js
@@ -1,10 +1,11 @@
+
 const assert = require('assert');
 //”cassandra-driver” is in the node_modules folder. Redirect if necessary.
 const cassandra = require('cassandra-driver');
 //Replace Username and Password with your cluster settings
-const authProvider = new cassandra.auth.PlainTextAuthProvider('iccassandra', 'dd7f9212d0acf03d923d7f11ac9cf65a');
+const authProvider = new cassandra.auth.PlainTextAuthProvider(process.env.usr, process.env.pw);
 //Replace PublicIP with the IP addresses of your clusters
-const contactPoints = ["20.203.149.23", "20.203.189.87"];
+const contactPoints = [process.env.node1, process.env.node2];
 var client = new cassandra.Client({ contactPoints: contactPoints, authProvider: authProvider, localDataCenter: 'AZURE_SWITZERLAND_NORTH', keyspace: 'grocery' });
 
 //Execute the queries 
