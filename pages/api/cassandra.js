c366300cd259184e5ca99fdaeeb60a87430921cd
StefanVinica <stefannikolovski996@gmail.com>
2022-01-12 09:24:34 -0500

    new route

--- pages/api/cassandra.js
+++ pages/api/cassandra.js
@@ -10,14 +10,14 @@ var client = new cassandra.Client({ contactPoints: contactPoints, authProvider: 
 //Execute the queries 
 var query = 'SELECT * FROM grocery.fruit_stock;';
 let response
-async function call (){
- await new Promise((resolve, reject) => {
+async function get() {
+  await new Promise((resolve, reject) => {
     client.execute(query)
       .then(res => {
-        if(!res){
+        if (!res) {
           response = 'error'
           reject()
-        }else{
+        } else {
           response = res
           resolve()
         }
@@ -25,11 +25,29 @@ async function call (){
   })
 }
 
+async function post(number, name, price) {
+  await new Promise((resolve, reject) => {
+    client.execute(`INSERT INTO grocery.fruit_stock (item_id, name, price_p_item) VALUES ('d${number}','${name}',${price});`)
+    .then(res => {
+      if (!res) {
+        reject()
+      } else {
+        resolve()
+      }
+    })
+  })
+}
 
 export default async function handler(req, res) {
-  await call()
-  .then(()=>{
-    if(response){
-    res.status(200).json(response)}
-  })
+  if (req.method === 'POST') {
+    await post(req.body.number, req.body.name, req.body.price)
+    .then(res.status(200).json('Sucsess'))
+  } else {
+    await get()
+      .then(() => {
+        if (response) {
+          res.status(200).json(response)
+        }
+      })
+  }
 }
