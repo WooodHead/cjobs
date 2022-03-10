2cb80eda04b2dcd38063202a6925315ff711c916
kire21 <kire.zdraveski@gmail.com>
2022-03-10 15:02:54 +0100

    tehnologies new ui

--- components/CompanyCard.js
+++ components/CompanyCard.js
@@ -4,9 +4,18 @@ import {
   Grid,
   Typography,
   Box,
-  Chip,
+  Paper,
 } from "@material-ui/core";
 import classes from "../styles/CompanyCard.module.css";
+import { styled } from "@mui/material/styles";
+
+const Item = styled(Paper)(({ theme }) => ({
+  ...theme.typography.body2,
+  textAlign: "center",
+  color: theme.palette.text.secondary,
+  height: 60,
+  lineHeight: "60px",
+}));
 
 const CompanyCard = ({ companyInfo }) => {
   console.log(companyInfo);
@@ -16,42 +25,32 @@ const CompanyCard = ({ companyInfo }) => {
       <Grid container>
         <Grid item xs={12}>
           <Card className={classes.cardHeader}>
-            <Grid container>
-              <Grid item xs={6}>
-                <CardContent>
-                  <Typography variant="h2">Header</Typography>
-                  <Typography variant="h5">Location</Typography>
-                </CardContent>
-              </Grid>
-              <Grid item xs={6}>
-                <CardContent>
-                  <Typography>Description</Typography>
-                </CardContent>
-              </Grid>
+            <Grid item xs={6}>
+              <CardContent>
+                <Typography variant="h2">Header</Typography>
+                <Typography variant="h5">Location</Typography>
+              </CardContent>
+            </Grid>
+            <Grid item xs={6}>
+              <CardContent>
+                <Typography>Description</Typography>
+              </CardContent>
             </Grid>
           </Card>
         </Grid>
       </Grid>
 
-      <Grid container spacing={4}>
+      <Grid container spacing={2}>
         <Grid item xs={6}>
-          <Card>
-            <CardContent>
-              <Typography>Tehnologies</Typography>
-              <Typography>
-                {companyInfo.hits.hits[0]._source.clearbit_tech.map((item) => {
-                  return (
-                    <Chip
-                      variant="outlined"
-                      key={item}
-                      label={item}
-                      className={classes.cardChip}
-                    />
-                  );
-                })}
-              </Typography>
-              <Typography>Categories</Typography>
-            </CardContent>
+          <Card className={classes.cardTehnologiesCategories}>
+            <Typography>Tehnologies</Typography>
+            <Box className={classes.tehnologies}>
+              {companyInfo.hits.hits[0]._source.clearbit_tech.map((item) => (
+                <Item key={item} elevation={2}>
+                  {`${item}`}
+                </Item>
+              ))}
+            </Box>
           </Card>
         </Grid>
         <Grid item xs={6}>
