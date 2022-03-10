d190ef9ab27264a3761da2b9544db85ab7d5f306
StefanVinica <stefannikolovski996@gmail.com>
2022-03-10 04:16:52 -0500

    API Call

--- pages/company-view/index.js
+++ pages/company-view/index.js
@@ -1,11 +1,28 @@
 import CompanyCard from "../../components/CompanyCard";
 
-const CompanyView = () => {
+export const getStaticProps = async () => {
+  try {
+    const res = await fetch(`http://167.172.142.105:5000/api/company/StyleSeat`);
+    const companyInfo = await res.json();
+    return {
+      props: {
+        companyInfo,
+      },
+    };
+  } catch (error) {
+    console.error("Error fetching homepage data", error);
+  }
+};
+
+export default function CompanyView({ companyInfo }) {
+  console.log(companyInfo)
   return (
     <>
       <CompanyCard />
     </>
   );
-};
+}
 
-export default CompanyView;
+
+
+
