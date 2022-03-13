b745ea157a586256a00be8e98cb64a02ea0a4ce0
StefanVinica <stefannikolovski996@gmail.com>
2022-03-13 15:48:46 -0400

    Ready for Prod

--- pages/api/elasticSearch.js
+++ pages/api/elasticSearch.js
@@ -1,4 +1,4 @@
 const index_name = "cassandra_job_posts";
-const esEndpoint = `http://${process.env.API_ENDPOINT}/api/elasticsearch/${index_name}`;
+const esEndpoint = `http://167.172.142.105:5000/api/elasticsearch/${index_name}`;
 
 export default esEndpoint;
